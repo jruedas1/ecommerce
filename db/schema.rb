555658e05bb96e164f6305b0614c26b780accac3
Javier Ruedas <jruedas@twc.com>
2018-06-11 15:35:47 -0500

    basic carrierwave implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_194827) do
+ActiveRecord::Schema.define(version: 2018_06_11_203015) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_06_11_194827) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
