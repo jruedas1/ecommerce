ab0259024b914f8c53f7c15243e6b18afcb78286
Javier Ruedas <jruedas@twc.com>
2018-06-11 14:50:59 -0500

    scaffold Category and associate categories and products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_194354) do
+ActiveRecord::Schema.define(version: 2018_06_11_194827) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
