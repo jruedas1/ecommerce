6aae805e197e4590f66037a5c4db7468beb8b9be
Javier Ruedas <jruedas@twc.com>
2018-06-12 12:41:04 -0500

    create cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'cart/add_to_cart'
-  get 'cart/view_order'
-  get 'cart/checkout'
+  post 'add_to_cart' => 'cart#add_to_cart'
+  get 'view_order' => 'cart#view_order'
+  get 'checkout' => 'cart#checkout'
+
   devise_for :users
+  
   root 'storefront#all_items'
   
   get 'categorical' => 'storefront#items_by_category'
+  
   get 'branding' => 'storefront#items_by_brand'
   
   resources :categories
