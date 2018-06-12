d92ae4ab46adc05d261029bc3afa8486dcb6da4b
Javier Ruedas <jruedas@twc.com>
2018-06-12 14:05:27 -0500

    set up cancan and add role to devise sanitizers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
-	
+	protect_from_forgery with: :exception
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
 	before_action :categories, :brands
+
+	rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to main_app.product_url, :alert => "Not authorized!" }
+    end
+  end
+
+#Our category and brand methods are below
 
 	def categories
 		@categories = Category.order(:name)
@@ -8,5 +20,12 @@ class ApplicationController < ActionController::Base
 
 	def brands
 		@brands = Product.pluck(:brand).sort.uniq
+	end
+
+	protected
+
+	def configure_permitted_parameters
+	   devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+	   devise_parameter_sanitizer.permit(:account_update, keys: [:role])
 	end
 end
