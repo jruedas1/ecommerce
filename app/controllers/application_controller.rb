4b15caf448a10bd4db349f6b44833f242abc35e3
Javier Ruedas <jruedas@twc.com>
2018-06-12 10:57:12 -0500

    enable items by brand

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
 	
-	before_action :categories
+	before_action :categories, :brands
 
 	def categories
 		@categories = Category.order(:name)
+	end
+
+	def brands
+		@brands = Product.pluck(:brand).sort.uniq
 	end
 end
