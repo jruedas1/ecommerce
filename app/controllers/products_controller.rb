dc64bf76955d7bb9c03305101effcc9a479aeb6c
Javier Ruedas <jruedas@twc.com>
2018-06-12 09:47:22 -0500

    add devise and signin-signout buttons

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+
 
   # GET /products
   # GET /products.json
